c363a815c7bb025b2622bcf982232dee19f8f56d
lixiangyu-pos <58729399+lixiangyu-pos@users.noreply.github.com>
2021-12-18 19:45:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,34 +24,3 @@ echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >
 git clone https://github.com/iwrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone -b master --depth 1 https://github.com/kiddin9/luci-theme-edge package/new/luci-theme-edge
-function merge_package(){
-    pn=`echo $1 | rev | cut -d'/' -f 1 | rev`
-    find package/ -follow -name $pn -not -path "package/custom/*" | xargs -rt rm -r
-    if [ ! -z "$2" ]; then
-        find package/ -follow -name $2 -not -path "package/custom/*" | xargs -rt rm -r
-    fi
-
-    if [[ $1 == *'/trunk/'* || $1 == *'/branches/'* ]]; then
-        svn export $1
-    else
-        git clone --depth=1 --single-branch $3 $1
-        rm -rf $pn/.git
-    fi
-    mv $pn package/custom/
-}
-function drop_package(){
-    find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -r
-}
-function merge_feed(){
-    if [ ! -d "feed/$1" ]; then
-        echo >> feeds.conf.default
-        echo "src-git $1 $2" >> feeds.conf.default
-    fi
-    ./scripts/feeds update $1
-    ./scripts/feeds install -a -p $1
-}
-
-rm -rf package/custom; mkdir package/custom
-merge_package https://github.com/messense/aliyundrive-webdav/trunk/openwrt/aliyundrive-webdav
-merge_package https://github.com/messense/aliyundrive-webdav/trunk/openwrt/luci-app-aliyundrive-webdav
-merge_package https://github.com/small-5/luci-app-adblock-plus
